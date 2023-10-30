ad64f74f9006ae1ef91f6150f67ed190c4a0d189
Slayer-I-K <133854361+Slayer-I-K@users.noreply.github.com>
2023-10-30 23:20:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+let api = "https://script.google.com/macros/s/AKfycbzDWvZCyIlSkxonNlNGWa_JdLGOW-1i947UxB-e3i1XL-T2w3KtUK8CMYdXDGH8jlGMXw/exec";
+    let form = document.querySelector("form");
+    let add = document.querySelector(".add");
+    function addData() {
+        add.textContent="getting"
+        let obj = {
+                       todo:form[0,1].value
+        }
+        fetch(api,{
+            method:"POST",
+            body:JSON.stringify(obj)
+        })
+         .then(res=> res.text())
+         .then(data => {
+             alert(data)
+             add.textContent="Get"
+          form.reset();
+         });
+    }
+    
